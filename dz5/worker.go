a3f6d9c8a3b8ccd12f61309551dc5461303b825b
gena <gennady.borisenko@profitclicks.ru>
2020-01-13 20:51:50 +0400

    дописал альфу

--- dz5/worker.go
+++ dz5/worker.go
@@ -1,35 +1,64 @@
 package dz5
 
-import "time"
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
 
-var taskChan chan func() error
-var errChan chan error
-var closeChan chan bool
+func Run(tasks []func() error, n, m int) error {
 
-func Run(tasks []func() error, n, m int) {
-	taskChan = make(chan func() error, 10000)
-	errChan = make(chan error, 10000)
-	closeChan = make(chan bool)
+	taskCount := uint32(len(tasks))
+	taskChan := make(chan func() error, taskCount)
+	errChan := make(chan error, taskCount)
+	closeChan := make(chan bool)
+	wg := sync.WaitGroup{}
+
+	var completedHandlerCount uint32 = 0
+	wg.Add(n)
 	for i := 0; i < n; i++ {
-		go func() {
-			for true {
-				// можно сделать через атомик. Меньше кода будет)
-				select {
-				case <-closeChan:
-					return
-				default:
-				}
-
-				select {
-				case task := <-taskChan:
-					if err := task(); err != nil {
-						errChan <- err
-					}
-				default:
-					time.Sleep(time.Second)
-				}
-			}
-		}()
+		go runHandler(&wg, taskChan, errChan, closeChan, &completedHandlerCount)
 	}
 
+	go func(tasks []func() error) {
+		for i := range tasks {
+			taskChan <- tasks[i]
+		}
+	}(tasks)
+
+	countErrors := 0
+	for countErrors < m && atomic.LoadUint32(&completedHandlerCount) < taskCount {
+		select {
+		case <-errChan:
+			countErrors++
+		default:
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+	closeChan <- true
+	wg.Wait()
+	return nil
+}
+
+func runHandler(wg *sync.WaitGroup, taskChan chan func() error, errChan chan error, closeChan chan bool, completedCount *uint32) {
+	for true {
+		// можно сделать через атомик. Меньше кода будет)
+		select {
+		case <-closeChan:
+			wg.Done()
+			return
+		default:
+		}
+
+		select {
+		case task := <-taskChan:
+			if err := task(); err != nil {
+				errChan <- err
+			}
+			atomic.AddUint32(completedCount, 1)
+		default:
+			// TODO: через тики в for
+			time.Sleep(time.Second)
+		}
+	}
 }
