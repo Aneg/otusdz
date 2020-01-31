e21f7e5f91bed5d967748f96416e6f9156952d44
gena <gennady.borisenko@profitclicks.ru>
2020-01-31 11:41:32 +0400

    доделал тесты

--- dz6/copy_file.go
+++ dz6/copy_file.go
@@ -2,13 +2,11 @@ package dz6
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"os"
 )
 
 func CopyFile(from *os.File, to *os.File, offset, limit int64) error {
-	fmt.Println(limit)
 	fi, err := from.Stat()
 	if err != nil {
 		return err
@@ -21,15 +19,17 @@ func CopyFile(from *os.File, to *os.File, offset, limit int64) error {
 	if limit == 0 || offset+limit > fromSize {
 		limit = fromSize - offset
 	}
+
 	if _, err := from.Seek(offset, io.SeekStart); err != nil {
 		return err
 	}
-	fmt.Println(limit)
+
 	var N int64
-	N = 10000
-	b := make([]byte, N)
 	var currentCopy int64
 	var lenRead int
+
+	N = 1024
+	b := make([]byte, N)
 	for currentCopy = 0; currentCopy < limit && (currentCopy+offset) < fromSize; {
 		// есливдруг конец
 		if currentCopy+N > limit {
@@ -38,7 +38,7 @@ func CopyFile(from *os.File, to *os.File, offset, limit int64) error {
 		if lenRead, err = from.ReadAt(b, offset+currentCopy); err != nil {
 			return err
 		}
-		if _, err = to.WriteAt(b, offset+currentCopy); err != nil {
+		if _, err = to.WriteAt(b, currentCopy); err != nil {
 			return err
 		}
 		currentCopy += int64(lenRead)
--- dz6/copy_file_test.go
+++ dz6/copy_file_test.go
@@ -1,32 +1,102 @@
 package dz6
 
 import (
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
-func TestCopyFile(t *testing.T) {
+func TestCopyFile_with_limit_and_offset(t *testing.T) {
 	fromPath := "from.data"
 	toPath := "to.data"
 
+	if err := createFile(fromPath, 1024*1024*10); err != nil {
+		t.Error(err)
+	}
+
+	testCases := []struct {
+		limit  int64
+		offset int64
+		first  int
+		last   int
+		length int
+	}{
+		{
+			limit:  100,
+			offset: 101,
+			first:  49,
+			last:   50,
+			length: 100,
+		},
+		{
+			limit:  0,
+			offset: 100,
+			first:  49,
+			last:   50,
+			length: 1024*1024*10 - 100,
+		},
+		{
+			limit:  102,
+			offset: 0,
+			first:  49,
+			last:   50,
+			length: 102,
+		},
+		{
+			limit:  0,
+			offset: 0,
+			first:  49,
+			last:   50,
+			length: 1024 * 1024 * 10,
+		},
+	}
+
 	from, err := os.OpenFile(fromPath, os.O_RDWR, 0644)
 	if err != nil {
-		if os.IsNotExist(err) {
-			t.Error("IsNotExist")
-		}
 		t.Error("другие ошибки, например нет прав")
 	}
 	defer from.Close()
-	to, err := os.OpenFile(toPath, os.O_RDWR, 0644)
-	if err != nil {
-		if os.IsNotExist(err) {
-			t.Error("IsNotExist")
-		}
-		t.Error("другие ошибки, например нет прав")
-	}
-	defer to.Close()
 
-	if err := CopyFile(from, to, 100, 100); err != nil {
-		t.Error(err)
+	for num, testCase := range testCases {
+		to, err := os.Create(toPath)
+		if err != nil {
+			t.Error(err)
+		}
+		if err := CopyFile(from, to, testCase.offset, testCase.limit); err != nil {
+			t.Errorf("№ %d: %s", num, err)
+		}
+
+		b, err := ioutil.ReadAll(to)
+		if len(b) != int(testCase.length) {
+			t.Errorf("№ %d: Не верно скопировано: %d", num, len(b))
+		}
+		if b[0] != byte(testCase.first) && b[testCase.length-1] != byte(testCase.last) {
+			t.Errorf("№ %d: Не верно скопировано.", num)
+		}
+
+		to.Close()
+		_ = os.Remove(toPath)
 	}
+
+	_ = os.Remove(fromPath)
+}
+
+func createFile(path string, size int) error {
+	file, err := os.Create(path)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+
+	b := make([]byte, size)
+
+	for i := 0; i < size; i++ {
+		if i < 101 {
+			b[i] = 49
+		} else {
+			b[i] = 50
+		}
+	}
+	_, err = file.Write(b)
+	return err
 }
